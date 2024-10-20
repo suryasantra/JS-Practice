f8eb04f81b1d6df75ba22bd32d8ec615ff8f36d9
suryasantra <santrasurya70@gmail.com>
2024-10-20 18:06:12 +0000

    var not use

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 154236
+let accountMail = "surya@yahoo.com"
+var accountPassword = "123456789"
+accountCity ="Kolkata"
+
+// accountId = 3 (not allowed)
+
+console.log(accountId);
+console.table([accountId,accountMail,accountPassword,accountCity])
+
+accountMail = "santrasurya13@re-issue.com"
+accountPassword = "28242013"
+accountCity = "Pune"
+
+/*
+  prefer not use var 
+   because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountMail,accountPassword,accountCity])
