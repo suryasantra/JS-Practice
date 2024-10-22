65c231399337c5913588df22b03e6307cf8ec65e
suryasantra <santrasurya70@gmail.com>
2024-10-22 18:57:12 +0000

    conversion

---
+++ 01_basics/03_dataconversion.js
@@ -0,0 +1,10 @@
+let score = "44ab"
+
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInnumber);
+console.log(typeof score);
+console.log(score);
+console.log(valueInnumber);
