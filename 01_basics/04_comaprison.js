ef4e266ab42498ec4137ddc436b6f8123db355fb
Surya Santra <santrasurya70@gmail.com>
2025-01-04 00:48:02 +0530

    Update 04_comaprison.js

--- 01_basics/04_comaprison.js
+++ 01_basics/04_comaprison.js
@@ -7,3 +7,14 @@ console.log(2 != 1)
 
 console.log("2" > 1)
 console.log("02" >  1)
+console.log(null >  0)
+console.log("hello")
+console.log(null ==  0)
+console.log(null >=  0)
+console.log(null <  1)
+console.log(null >  1)
+
+// ===
+
+console.log("2"=== 2)
+
