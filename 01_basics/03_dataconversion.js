6d6a0d650208d70d6b66fa4d57d8951be893d899
Surya Santra <santrasurya70@gmail.com>
2025-01-03 22:57:32 +0530

    Update 03_dataconversion.js

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -3,8 +3,15 @@ let score = "44ab"
 
 console.log(typeof score);
 
-let valueInNumber = Number(score)
+let valueInnumber = Number(score) //for changes data type using capital "N" its mean Number
 console.log(typeof valueInnumber);
 console.log(typeof score);
 console.log(score);
 console.log(valueInnumber);
+
+
+/*
+  "44" ==>44
+  "44ab"==>NaN(Not an Number)
+  
+*/
