5dbf33745b9d094282aee4bde606eba5906e686f
Surya Santra <santrasurya70@gmail.com>
2025-01-03 23:06:28 +0530

    Update 03_dataconversion.js

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -13,5 +13,20 @@ console.log(valueInnumber);
 /*
   "44" ==>44
   "44ab"==>NaN(Not an Number)
+  true=>1
+  false=>0
   
 */
+
+let islogedin = 1
+let booleanislogedin = Boolean(islogedin)
+console.log(booleanislogedin)
+
+// 1=> true and 0=> false
+
+let somenumber = 44
+console.log(typeof (somenumber))
+let stringsomenumber = String(somenumber)
+ 
+console.log(stringsomenumber)
+console.log(typeof (stringsomenumber))
