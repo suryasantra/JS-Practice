25cb5019c32cb85d156eefbd53e4a5694bb0c08b
Surya Santra <santrasurya70@gmail.com>
2025-01-03 22:50:58 +0530

    Update 02_datatyps.js

--- 01_basics/02_datatyps.js
+++ 01_basics/02_datatyps.js
@@ -1,3 +1,18 @@
 "Use strict"; //treat all JS code as newer version
 
-//alert(4 + 4) we are using NodeJs, not browser                                         
+//alert(4 + 4) we are using NodeJs, not browser  
+
+/*
+number =>
+bigint=>
+boolean=> true/false
+null=>
+undifined
+symbol=>uniqe
+
+*/
+
+//Object
+
+console.log(typeof null)//object
+console.log(typeof undifined)//undifined
